fcf76a722c6326825e627b165747d93da87f671a
Yuriy Borys <iam618384@gmail.com>
2021-03-12 11:11:44 +0200

    Add preload fonts

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,43 @@
+import Head from "next/head";
+
 export const Layout = ({ children }) => (
-    <>
-        <div>Header</div>
-        {children}
-        <div>Footer</div>
-    </>
+    <div>
+        <Head>
+            <link
+                rel="preload"
+                href="/fonts/Montserrat/Montserrat-Light.ttf"
+                as="font"
+                crossOrigin="" />
+            <link
+                rel="preload"
+                href="/fonts/Montserrat/Montserrat-Regular.ttf"
+                as="font"
+                crossOrigin="" />
+            <link
+                rel="preload"
+                href="/fonts/Montserrat/Montserrat-Medium.ttf"
+                as="font"
+                crossOrigin="" />
+            <link
+                rel="preload"
+                href="/fonts/Montserrat/Montserrat-SemiBold.ttf"
+                as="font"
+                crossOrigin="" />
+            <link
+                rel="preload"
+                href="/fonts/Montserrat/Montserrat-Bold.ttf"
+                as="font"
+                crossOrigin="" />
+            <link
+                rel="preload"
+                href="/fonts/MonotypeCorsiva/Monotype-Corsiva.ttf"
+                as="font"
+                crossOrigin="" />
+        </Head>
+        <body>
+            <div>Header</div>
+            {children}
+            <div>Footer</div>
+        </body>
+    </div>
 )
