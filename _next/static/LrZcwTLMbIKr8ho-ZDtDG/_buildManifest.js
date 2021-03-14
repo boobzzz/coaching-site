c408fe76483233232a61f8983f4de091b5159313
boobzzz <35819774+boobzzz@users.noreply.github.com>
2021-03-14 20:01:47 +0000

    Deploying to gh-pages from @ boobzzz/coaching-site@03a81a8e9e3412d4989eb0e3e894279f0b7a5839 🚀

--- _next/static/LrZcwTLMbIKr8ho-ZDtDG/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F0d6c265634a29f827ca6.css","static\u002Fchunks\u002Fpages\u002Findex-dc0f2180cbed592fe53f.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-94615ce0fdbd40e5f4c7.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
