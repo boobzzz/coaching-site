0c78a990fcceb2ef82921443ef8024eae00325c7
Yuriy Borys <iam618384@gmail.com>
2021-03-15 00:02:10 +0200

    Update font

--- components/Layout.js
+++ components/Layout.js
@@ -14,55 +14,55 @@ export const Layout = ({ children }) => (
                 href={`${prefix}/fonts/Montserrat/Montserrat-ExtraLight.ttf`}
                 // href="/fonts/Montserrat/Montserrat-ExtraLight.ttf"
                 as="font"
-                crossOrigin="" />
+                crossOrigin="anonymous" />
             <link
                 rel="preload"
                 href={`${prefix}/fonts/Montserrat/Montserrat-Light.ttf`}
                 // href="/fonts/Montserrat/Montserrat-Light.ttf"
                 as="font"
-                crossOrigin="" />
+                crossOrigin="anonymous" />
             <link
                 rel="preload"
                 href={`${prefix}/fonts/Montserrat/Montserrat-Regular.ttf`}
                 // href="/fonts/Montserrat/Montserrat-Regular.ttf"
                 as="font"
-                crossOrigin="" />
+                crossOrigin="anonymous" />
             <link
                 rel="preload"
                 href={`${prefix}/fonts/Montserrat/Montserrat-Medium.ttf`}
                 // href="/fonts/Montserrat/Montserrat-Medium.ttf"
                 as="font"
-                crossOrigin="" />
+                crossOrigin="anonymous" />
             <link
                 rel="preload"
                 href={`${prefix}/fonts/Montserrat/Montserrat-SemiBold.ttf`}
                 // href="/fonts/Montserrat/Montserrat-SemiBold.ttf"
                 as="font"
-                crossOrigin="" />
+                crossOrigin="anonymous" />
             <link
                 rel="preload"
                 href={`${prefix}/fonts/Montserrat/Montserrat-Bold.ttf`}
                 // href="/fonts/Montserrat/Montserrat-Bold.ttf"
                 as="font"
-                crossOrigin="" />
+                crossOrigin="anonymous" />
             <link
                 rel="preload"
                 href={`${prefix}/fonts/Montserrat/Montserrat-ExtraBold.ttf`}
                 // href="/fonts/Montserrat/Montserrat-ExtraBold.ttf"
                 as="font"
-                crossOrigin="" />
+                crossOrigin="anonymous" />
             <link
                 rel="preload"
                 href={`${prefix}/fonts/Montserrat/Montserrat-Black.ttf`}
                 // href="/fonts/Montserrat/Montserrat-Black.ttf"
                 as="font"
-                crossOrigin="" />
+                crossOrigin="anonymous" />
             <link
                 rel="preload"
                 href={`${prefix}/fonts/Monotype/Monotype-Corsiva.ttf`}
                 // href="/fonts/Monotype/Monotype-Corsiva.ttf"
                 as="font"
-                crossOrigin="" />
+                crossOrigin="anonymous" />
             <title>Liliya Luts | professional certified coach</title>
             <meta name="description" content="Lilia Luts - organizational consultant, business coach, transformational coach, NLP Master, member of the International Coaching Federation (ICF)" />
             <meta name="keywords" content="coach, business coach, transformational coach, NLP, ICF" />
